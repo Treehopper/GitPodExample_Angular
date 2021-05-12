54217dc0beca49a695f39b2c9350259d0447c4f0
Treehopper <github.com@hohenegger.eu>
2021-05-12 17:05:39 +0000

    Import ngx-demo project.
    See: https://stackblitz.com/edit/ngx-graph-custom-layout-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule,  } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, MaterialModule, NgxGraphModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
